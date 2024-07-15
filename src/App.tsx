b71a1753752a718a9a1294a15a8122723ebfb5e3
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-15 19:15:06 -0300

    Add global css to provide consistent styling variables across the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
 import Footer from './components/Footer/Footer';
+import './styles.css';
 
 function App() {
   return (
