69fc88fdec86935885c91f6cb5bdce905577b9f9
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-15 20:55:45 -0300

    Add MenuIcon component for collapsible navigation bar

---
+++ src/components/Header/NavBar/MenuIcon/MenuIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./MenuIcon.module.css";
+
+const MenuIcon: React.FC = () => {
+  return (
+    <div className={styles.menuIconContainer}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+};
+
+export default MenuIcon;
