69254ee2319e2b53da0dc5469c9c2069a2f17abf
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-15 19:34:29 -0300

    Add 'colorStyle' property to Logo to choose correct color scheme
    Applicatioin needs to be able to render the logo in two different
    color schemes, but current implementation lacked a way to select
    between the two.
    Added 'colorStyle' property to solve this issue. Possible values
    'light' and 'dark' choose between the two color schemes. The prop
    refers to the color of the background

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./Logo.module.css";
 
-const Logo: React.FC = () => {
-  return <div className={`${styles.logo} ${styles.logoDark}`}>T</div>
+interface LogoProps {
+  colorStyle: 'bright' | 'dark';
+}
+
+const Logo: React.FC<LogoProps> = ({ colorStyle }) => {
+  const colorClass: string = colorStyle === 'bright' ? styles.logoBright : styles.logoDark;
+
+  return <div className={`${styles.logo} ${colorClass}`}>T</div>
 }
 
 export default Logo;
