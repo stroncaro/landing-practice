a686dbdf59490d59bcc2bc75489106cf46ffd474
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-15 23:48:20 -0300

    Add Hero section

--- src/components/Content/Hero/Hero.tsx
+++ src/components/Content/Hero/Hero.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Button from "../../shared/Button";
+import ambulance from "../../../assets/svg/Ambulance.svg"
 
 const Hero: React.FC = () => {
   return (
-    <section>
-      <h1>Hero</h1>
+    <section id='hero'>
+      <div>
+        <h1>Virtual Healthcare for you</h1>
+        <p>
+          Trafalgar provides progressive, and
+          affordable healthcare, accessible
+          on mobile and online for everyone
+        </p>
+        <Button backgroundStyle="primary" text="Consult today" />
+      </div>
+      <img src={ambulance} alt="Ambulance ready to aid you!"/>
     </section>
   )
 }
