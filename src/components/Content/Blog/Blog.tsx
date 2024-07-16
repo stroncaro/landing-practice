45cf8c7e223b3bd43bbd4cfbd46176bf03724bb4
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-16 00:56:44 -0300

    Add Blog section content

--- src/components/Content/Blog/Blog.tsx
+++ src/components/Content/Blog/Blog.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import Article, { ArticleProps } from "./Article/Article";
+import Button from "../../shared/Button";
+
+const jpgModules = import.meta.glob("../../../assets/jpg/*.jpg", { eager: true });
+
+const jpgs: string[] = Object.values(jpgModules).map(
+  (module) => (module as { default: string }).default
+);
+
+const articles: ArticleProps[] = [
+  { imgSrc: jpgs[0], imgAlt: "filler", title: "Search doctor", text: "Choose your doctor from thousands of specialist, general, and trusted hospitals"},
+  { imgSrc: jpgs[1], imgAlt: "filler", title: "Online Pharmacy", text: "Buy your medicines with our mobile application with a simple delivery system"},
+  { imgSrc: jpgs[2], imgAlt: "filler", title: "Consultation", text: "Free consultation with our trusted doctors and get the best recomendations"},
+]
 
 const Blog: React.FC = () => {
   return (
     <section>
-      <h1>Blog</h1>
+      <h1>Check out our latest article</h1>
+      <hr></hr>
+      <div className="articles">
+        {articles.map((article) => (
+          <Article
+            imgSrc={article.imgSrc}
+            imgAlt={article.imgAlt}
+            title={article.title}
+            text={article.text} />
+        ))}
+      </div>
+      <Button backgroundStyle="white" text="View all" />
     </section>
   )
 };
