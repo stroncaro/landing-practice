21e3550d457052abc04cf94000a20c7b36c93dd2
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-15 21:15:40 -0300

    Add MenuIcon to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Logo from '../shared/Logo'
+// import NavBar from './NavBar/NavBar';
+import MenuIcon from './MenuIcon/MenuIcon';
+import styles from './Header.module.css';
 
 const Header: React.FC = () => {
   return (
-    <header id='page-header'>
+    <header id='page-header' className={styles.header}>
       <Logo backgroundStyle='primary'/>
+      <MenuIcon />
+      {/* TODO: make collapsible navbar appear when menu button is clicked */}
+      {/* <NavBar /> */}
     </header>
   )
 }
