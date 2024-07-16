15dc268a13598db958d2db98f524c2abcc29582b
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-16 03:32:00 -0300

    Add Footer content and style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Logo from "../shared/Logo";
 
+import styles from "./Footer.module.css";
+
 const Footer: React.FC = () => {
   return (
-    <footer id='page-footer'>
-      <h1>Footer</h1>
-      <Logo backgroundStyle="white"/>
+    <footer id='page-footer' className={styles.footer}>
+      <div>
+        <Logo backgroundStyle="white"/>
+        <p>
+          Trafalgar provides progressive, and
+          affordable healthcare, accessible on
+          mobile and online for everyone
+        </p>
+        <p>©Trafalgar PTY LTD 2020. All rights reserved</p>
+      </div>
+      <div>
+        <h1>Company</h1>
+        <ul>
+          <li><a>About</a></li>
+          <li><a>Testimonials</a></li>
+          <li><a>Find a doctor</a></li>
+          <li><a>Apps</a></li>
+        </ul>
+      </div>
     </footer>
   )
 }
